aebb6d967d1a9d65de55b64ae24394aeb129f88b
Kai <77860646+AceSardonyx@users.noreply.github.com>
2023-08-30 13:39:17 -0400

    aestetics :sparkles:
    stupid stuff I fail to do lol

---
+++ bullshittening/particles/js/app.js
@@ -0,0 +1,11 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+
+
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+
+
